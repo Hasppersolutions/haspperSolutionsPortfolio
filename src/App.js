9801a81cbe92f20e15cd4c6dfaf78689c6eb987e
roshan4798 <manishjha4798@gmail.com>
2024-10-01 01:31:05 +0530

    contact-us

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { NavBar } from "./components/NavBar";
 import Banner from "./components/Banner"
 import Section from "./components/Section"
 import ExpertServices from './components/ExpertServices';
+import { Contact } from "./components/Contact";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
@@ -12,6 +13,7 @@ function App() {
       <Banner />
       <Section/>
       <ExpertServices/>
+      <Contact />
     </div>
   );
 }
