af77e5ac864c4946a183c12d553d4ca296f3c7ab
Bobby Choudhary <bobby.choudhary4700@gmail.com>
2024-10-19 02:19:56 +0530

    changes in ai tool feature

--- src/Pages/ArtificialIntelligence.jsx
+++ src/Pages/ArtificialIntelligence.jsx
@@ -5,6 +5,11 @@ import AiDescription from "../components/Services/AiDescription";
 import ServiceBackgroudImage from "../assets/img/ServiceBackgroudImage.png";
 import AiBannerIcon from "../assets/img/AiBannerIcon.png";
 import AiTool from "../components/Services/Ai/AiTool";
+import npl from "../assets/img/npl.png";
+import healthCare from "../assets/img/healthCare.png";
+import dataEngineer from "../assets/img/dataEngineer.png";
+import generativeAi from "../assets/img/generativeAi.png";
+import recommendationSystem from "../assets/img/recommendationSystem.png";
 
 const ArtificialIntelligence = () => {
   return (
@@ -16,7 +21,7 @@ const ArtificialIntelligence = () => {
       >
         <Banner data={bannerTextData} />
         <AiDescription />
-        <AiTool />
+        <AiTool data={toolData} />
       </Box>
     </>
   );
@@ -32,4 +37,45 @@ const bannerTextData = {
     "Step into the future of business with our comprehensive Artificial Intelligence Solutions, propelling you toward unrivaled success.",
   // button: "Get Free Demo", //uncomment to add button to banner
   // link: "link" //uncomment to add link to button
-}
+};
+
+const toolData = {
+  heading: "AI Tools Simplifying",
+  subHeading: "Business Complexities",
+  description:
+    "Put your challenges to rest with our expertise in developing advanced AI solutions.",
+  uppeCardData: [
+    {
+      image: npl,
+      text: "Natural Language Processing for E-Commerce",
+      hoverText:
+        "Maximize consumer engagement with our NLP technology, designed to boost the e-commerce experience.",
+    },
+    {
+      image: healthCare,
+      text: "Computer Vision for Healthcare",
+      hoverText:
+        "With our AI-powered computer vision solutions, enhance patient care and diagnosis accuracy.",
+    },
+  ],
+  belowCardData: [
+    {
+      image: recommendationSystem,
+      text: "Recommendation Engine for Media",
+      hoverText:
+        "Grow user engagement with personalized content using our AI-based recommendation algorithms.",
+    },
+    {
+      image: generativeAi,
+      text: "Generative AI for Content Creation",
+      hoverText:
+        "Drive uniqueness and creativity in content with our advanced generative AI systems.",
+    },
+    {
+      image: dataEngineer,
+      text: "Data Engineering for Market Research",
+      hoverText:
+        "We structure and organize your data for insightful and data-driven decision-making. Harness the power of data for an evidence-based business strategy.",
+    },
+  ],
+};
--- src/components/Services/Ai/AiTool.jsx
+++ src/components/Services/Ai/AiTool.jsx
@@ -1,172 +1,242 @@
 import { Box, Container, Grid2 as Grid, Typography } from "@mui/material";
-import npl from "../../../assets/img/npl.png";
-import healthCare from "../../../assets/img/healthCare.png";
-import dataEngineer from "../../../assets/img/dataEngineer.png";
-import generativeAi from "../../../assets/img/generativeAi.png";
-import recommendationSystem from "../../../assets/img/recommendationSystem.png";
-const AiTool = () => {
+const AiTool = ({ data }) => {
   return (
     <>
-      <Box>
+      <Box
+        sx={{
+          margin: "50px 0px",
+        }}
+      >
         <Container>
-          <Grid container>
-            <Grid size={{ xs: 12, sm: 6 }}>
-              <Box>
-                <Typography
-                  component={"h1"}
-                  sx={{
-                    fontSize: { xs: "22px", sm: "28px", md: "36px" },
-                    fontWeight: 800,
-                    color: "#1a1a1a",
-                    lineHeight: 1.2,
-                  }}
-                >
-                  AI Tools Simplifying
-                </Typography>
-                <Typography
-                  sx={{
-                    fontSize: { xs: "22px", sm: "28px", md: "36px" },
-                    fontWeight: 800,
-                    color: "#c75425",
-                    mb: 2,
-                    lineHeight: 1.2,
-                  }}
-                >
-                  Business Complexities
-                </Typography>
-                <Typography
-                  sx={{
-                    fontSize: { xs: "12px", sm: "19px" },
-                    color: "#000",
-                    marginTop: "25px",
-                    width: "fit-content",
-                  }}
-                >
-                  Put your challenges to rest with our expertise in developing
-                  advanced AI solutions.
-                </Typography>
-              </Box>
-            </Grid>
-            <Grid size={{ xs: 12, sm: 6 }}>
-              <Grid container spacing={{xs: 0, md: 5}} rowGap={{xs: 5, md: 0}}>
-                <Grid
-                  size={{ xs: 12, sm: 6 }}
-                  sx={{
-                    borderRight: "2px solid rgba(0, 0, 0, 0.11)",
-                    borderBottom: "2px solid rgba(0, 0, 0, 0.11)",
-                  }}
-                >
-                  <Box
+          <Grid container rowGap={{ xs: 5, sm: 6 }}>
+            <Grid size={{ xs: 12, md: 6 }}>
+              <Box
+                sx={{
+                  height: { xs: 250, sm: 300 },
+                  display: "flex",
+                  alignItems: "center",
+                  justifyContent: "center",
+                }}
+              >
+                <Box>
+                  <Typography
+                    component={"h1"}
                     sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
+                      fontSize: { xs: "22px", sm: "28px", md: "36px" },
+                      fontWeight: 800,
+                      color: "#1a1a1a",
+                      lineHeight: 1.2,
                     }}
                   >
-                    <img
-                      src={npl}
-                      alt="Consulting Service"
-                      style={{ width: "35%", height: "35%" }}
-                    />
-                  </Box>
+                    {data.heading}
+                  </Typography>
                   <Typography
                     sx={{
-                      fontSize: { xs: "0.8rem", sm: "1.2rem" },
+                      fontSize: { xs: "22px", sm: "28px", md: "36px" },
+                      fontWeight: 800,
+                      color: "#c75425",
+                      mb: 2,
+                      lineHeight: 1.2,
+                    }}
+                  >
+                    {data.subHeading}
+                  </Typography>
+                  <Typography
+                    sx={{
+                      fontSize: { xs: "16px", sm: "19px" },
                       color: "#000",
                       marginTop: "25px",
-                      padding: "0 50px",
-                      // width: "fit-content",
+                      width: "fit-content",
                     }}
                   >
-                    Natural Language Processing for E-Commerce
+                    {data.description}
                   </Typography>
-                </Grid>
-                <Grid
-                  size={{ xs: 12, sm: 6 }}
-                  sx={{
-                    borderRight: "2px solid rgba(0, 0, 0, 0.11)",
-                    borderBottom: "2px solid rgba(0, 0, 0, 0.11)",
-                  }}
-                >
-                  <Box
-                    sx={{
-                      display: "flex",
-                      justifyContent: "center",
-                      alignItems: "center",
-                    }}
-                  >
-                    <img
-                      src={healthCare}
-                      alt="Consulting Service"
-                      style={{ width: "35%", height: "35%" }}
-                    />
-                  </Box>
-                </Grid>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid size={{ xs: 12, md: 6 }}>
+              <Grid container rowGap={{ xs: 2, md: 0 }}>
+                {data?.uppeCardData.map((item, index) => (
+                  <Grid size={{ xs: 12, sm: 6 }}>
+                    <Box
+                      sx={{
+                        borderRight: {
+                          sm: "2px solid rgba(0, 0, 0, 0.11)",
+                          md: "2px solid rgba(0, 0, 0, 0.11)",
+                        },
+                        borderBottom: {
+                          sm: "2px solid rgba(0, 0, 0, 0.11)",
+                          md: "2px solid rgba(0, 0, 0, 0.11)",
+                        },
+                        boxShadow: {
+                          xs: "0 1px 4px 0 rgba(0, 0, 0, .16)",
+                          sm: "none",
+                        },
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        padding: "20px",
+                        position: "relative",
+                        height: { xs: 250, sm: 300 },
+                        overflow: "hidden",
+                        ":hover": {
+                          ".overLayBox": {
+                            opacity: 1,
+                            top: 0,
+                            transition: "all 400ms cubic-bezier(0, 0, .2, 1)",
+                          },
+                        },
+                      }}
+                    >
+                      <Box>
+                        <Box
+                          sx={{
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                          }}
+                        >
+                          <img
+                            src={item.image}
+                            alt="Consulting Service"
+                            style={{ width: "80px" }}
+                          />
+                        </Box>
+                        <Typography
+                          sx={{
+                            fontSize: { xs: "18px" },
+                            color: "#337AB7",
+                            marginTop: "15px",
+                            textAlign: "center",
+                          }}
+                        >
+                          {item.text}
+                        </Typography>
+                        <Box
+                          className="overLayBox"
+                          sx={{
+                            position: "absolute",
+                            top: "100%",
+                            left: 0,
+                            width: "100%",
+                            height: "100%",
+                            background: "#337AB7",
+                            textAlign: "center",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            padding: "0 25px 25px",
+                            opacity: 0,
+                          }}
+                        >
+                          <Typography
+                            sx={{
+                              fontWeight: { xs: 400, sm: 400 },
+                              color: "#ffffff",
+                            }}
+                          >
+                            {item.hoverText}
+                          </Typography>
+                        </Box>
+                      </Box>
+                    </Box>
+                  </Grid>
+                ))}
               </Grid>
             </Grid>
           </Grid>
-          <Grid container spacing={{xs: 0, md: 5}} rowGap={{xs: 5, md: 0}}>
-            <Grid
-              size={{ xs: 12, sm: 3 }}
-              sx={{
-                borderRight: "2px solid rgba(0, 0, 0, 0.11)",
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  src={recommendationSystem}
-                  alt="Consulting Service"
-                  style={{ width: "35%", height: "35%" }}
-                />
-              </Box>
-            </Grid>
-            <Grid
-              size={{ xs: 12, sm: 3 }}
-              sx={{
-                borderRight: "2px solid rgba(0, 0, 0, 0.11)",
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  src={generativeAi}
-                  alt="Consulting Service"
-                  style={{ width: "35%", height: "35%" }}
-                />
-              </Box>
-            </Grid>
-            <Grid
-              size={{ xs: 12, sm: 3 }}
-              sx={{
-                borderRight: "2px solid rgba(0, 0, 0, 0.11)",
-              }}
-            >
-              <Box
-                sx={{
-                  display: "flex",
-                  justifyContent: "center",
-                  alignItems: "center",
-                }}
-              >
-                <img
-                  src={dataEngineer}
-                  alt="Consulting Service"
-                  style={{ width: "35%", height: "35%" }}
-                />
-              </Box>
-            </Grid>
-            <Grid size={{ xs: 12, sm: 3 }}></Grid>
+          <Grid
+            container
+            rowGap={{ xs: 2, sm: 0 }}
+            sx={{ marginTop: { xs: 2, md: 0, sm: 0 } }}
+          >
+            {data?.belowCardData?.map((item, index) => (
+              <Grid size={{ xs: 12, sm: 6, md: 3 }}>
+                <Box
+                  sx={{
+                    borderRight: {
+                      sm: "2px solid rgba(0, 0, 0, 0.11)",
+                      md: "2px solid rgba(0, 0, 0, 0.11)",
+                    },
+                    borderBottom: {
+                      sm: "2px solid rgba(0, 0, 0, 0.11)",
+                      md: "2px solid rgba(0, 0, 0, 0.11)",
+                    },
+                    boxShadow: {
+                      xs: "0 1px 4px 0 rgba(0, 0, 0, .16)",
+                      sm: "none",
+                    },
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    padding: "20px",
+                    position: "relative",
+                    height: { xs: 250, sm: 300 },
+                    overflow: "hidden",
+                    ":hover": {
+                      ".overLayBox": {
+                        opacity: 1,
+                        top: 0,
+                        transition: "all 400ms cubic-bezier(0, 0, .2, 1)",
+                      },
+                    },
+                  }}
+                >
+                  <Box>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                      }}
+                    >
+                      <img
+                        src={item.image}
+                        alt="Consulting Service"
+                        style={{ width: "80px" }}
+                      />
+                    </Box>
+                    <Typography
+                      sx={{
+                        fontSize: { xs: "18px" },
+                        color: "#337AB7",
+                        marginTop: "15px",
+                        textAlign: "center",
+                      }}
+                    >
+                      {item.text}
+                    </Typography>
+                    <Box
+                      className="overLayBox"
+                      sx={{
+                        position: "absolute",
+                        top: "100%",
+                        left: 0,
+                        width: "100%",
+                        height: "100%",
+                        background: "#337AB7",
+                        textAlign: "center",
+                        display: "flex",
+                        alignItems: "center",
+                        justifyContent: "center",
+                        padding: "0 25px 25px",
+                        opacity: 0,
+                      }}
+                    >
+                      <Typography
+                        sx={{
+                          fontWeight: { xs: 400, sm: 400 },
+                          color: "#ffffff",
+                        }}
+                      >
+                        {item.hoverText}
+                      </Typography>
+                    </Box>
+                  </Box>
+                </Box>
+              </Grid>
+            ))}
+            <Grid size={{ xs: 12, sm: 6, md: 3 }}></Grid>
           </Grid>
         </Container>
       </Box>
