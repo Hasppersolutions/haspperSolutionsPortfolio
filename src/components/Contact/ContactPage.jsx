f779664d6053d148ab2e6cca466ec52f91071304
Bobby Choudhary <bobby.choudhary4700@gmail.com>
2024-10-30 10:46:16 +0530

    changes in contact us footer

--- src/components/Contact/ContactPage.jsx
+++ src/components/Contact/ContactPage.jsx
@@ -3,18 +3,21 @@ import { ContactIntro } from "./ContactIntro";
 import { Contact } from "../Homepage/Contact";
 import { Box } from "@mui/material";
 import ServicesAndStaffing from "../../components/Homepage/ServicesAndStaffing";
+import Mailus from "../../components/Homepage/Mailus";
 
 export const ContactPage = () => {
   return (
     <>
       <ContactIntro />
       <Contact />
+      <Mailus />
       <Box
         sx={{
           backgroundColor: "#fff",
           mt: {xs: 5, sm: 8},
         }}
       >
+
         <ServicesAndStaffing />
       </Box>
     </>
